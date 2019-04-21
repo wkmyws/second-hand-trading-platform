3e778637169c1857b1ddf356b36e6f6800f4efae
Rabi <228355733@qq.com>
2019-04-21 18:23:59 +0800

    update 4.21
    删了好多东西

--- call_board/pages/board/detail/detail.js
+++ call_board/pages/board/detail/detail.js
@@ -5,11 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
+    img_url: [
+      '/images/test/1.png',
+      '/images/test/2.jpeg',
+      '/images/test/3.jpg',
+      '/images/test/4.jpg',
+      '/images/test/5.jpg',
+      '/images/test/6.jpg'
+    ],
     popup: true
   },
 
   showPopup() {
     this.hidePopup(false);
+  },
+
+  /*图片预览 */
+  Preview(e) {
+    var idx = e.target.dataset.idx
+    var img_url = this.data.img_url
+    wx.previewImage({
+      current: img_url[idx], //当前预览的图片
+      urls: img_url //所有要预览的图片
+    })
   },
 
   /**
