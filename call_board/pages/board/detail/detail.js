5f2395f9a4053eb77922d07efef345080cea7775
Rabi <228355733@qq.com>
2019-04-28 19:23:58 +0800

    就先这样吧

--- call_board/pages/board/detail/detail.js
+++ call_board/pages/board/detail/detail.js
@@ -12,13 +12,17 @@ Page({
       "http://xcx.nau.edu.cn/images/adv2.jpg?",
       "http://xcx.nau.edu.cn/images/school1.jpg?",
       "http://xcx.nau.edu.cn/images/plus.png?"
+<<<<<<< HEAD
+    ]
+=======
     ],
+    SaveStatus: 'false', //收藏的状态
+    SaveNum: 10, //收藏的数量
+    ThumbStatus: 'false', //点赞的状态
+    ThumbNum: 20,//点赞的数量
     popup: true,
+>>>>>>> 2d5120fc29c6292f5a2ef6cbf7ed42fd7a98c423
     
-  },
-
-  showPopup() {
-    this.hidePopup(false);
   },
 
   /*图片预览 */
@@ -39,6 +43,38 @@ Page({
       confirmText:"确认",
       cancelText:"取消"
     })
+  },
+  ChangeSave: function () {
+    var sta = this.data.SaveStatus
+    var num = this.data.SaveNum
+    if (sta) {
+      this.setData({
+        SaveStatus: false,
+        SaveNum: num + 1
+      })
+    } else {
+      this.setData({
+        SaveStatus: true,
+        SaveNum: num - 1
+      })
+    }
+  },
+
+  //点赞和取消点赞，点赞数量
+  ChangeThumb: function () {
+    var sta = this.data.ThumbStatus
+    var num = this.data.ThumbNum
+    if (sta) {
+      this.setData({
+        ThumbStatus: false,
+        ThumbNum: num + 1
+      })
+    } else {
+      this.setData({
+        ThumbStatus: true,
+        ThumbNum: num - 1
+      })
+    }
   },
 
   /**
