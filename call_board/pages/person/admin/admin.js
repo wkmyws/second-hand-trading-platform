891ab909f0ed17b90dff191eca31d9477530be17
wkmyws <45654909+wkmyws@users.noreply.github.com>
2019-08-10 22:19:12 +0800

    完善物品收藏 和 管理员审核

--- call_board/pages/person/admin/admin.js
+++ call_board/pages/person/admin/admin.js
@@ -1,3 +1,18 @@
+/*
+  passItem()函数用来提交审核结果，参数与 
+   https://dev.tencent.com/u/XFY9326/p/HighSchoolMarket/wiki/47
+  一致
+  eg:
+
+  this.passItem(0,1,true).then(()=>{
+    console.log('提交成功')
+  }).catch(()=>{
+    console.log('提交失败')
+  })
+
+ */
+var util = require('../../../utils/util.js');
+const app = getApp()
 Page({
 
   /**
@@ -5,39 +20,91 @@ Page({
    */
   data: {
     currentTab: 0,
-    SaveStatus: 'false', //收藏的状态
-    SaveNum: 10, //收藏的数量
-    ThumbStatus: 'false', //点赞的状态
-    ThumbNum: 20,//点赞的数量
+    goods:[],
+    boards:['test'],
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    //获取待审核商品
+    new Promise((resolve,reject)=>{
+      var timestamp = Date.parse(new Date());
+      timestamp = String(timestamp / 1000);
+      var data = JSON.stringify({ amount: 10, summary_sub: 10, type: 0 })
+      data = util.base64_encode(data)
+      var sign = util.sha1(data + timestamp + app.globalData.user_info.user_id)
+      //request
+      wx.request({
+        url: app.globalData.URL + 'manage/getWaitCheckList.php',
+        data: {
+          "version": 1,
+          "time": timestamp,
+          "data": data,
+          "sign": sign,
+          "token": app.globalData.token,
+        },
+        method: 'POST',
+        header: {
+          "content-type": "application/json"
+        },
+        success: res => {
+          console.log(res)
+          if (res.data.status == 1) return reject();
+          res = JSON.parse(util.base64_decode(res.data.data))
+          this.setData({
+            goods: res
+          })
+          return resolve();
+        }
+      })//end request
+    }).then(()=>{
+      console.log('emm')
+    }).catch(()=>{
+      wx.showToast({
+        title: '所在用户组权限不够',
+        icon: 'none',
+        duration: 4000
+      })
+
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  passItem:function(type,info_id,check_pass,check_conclusion){
+    return new Promise((resolve,reject)=>{
+      var timestamp = Date.parse(new Date());
+      timestamp = String(timestamp / 1000);
+      var data = JSON.stringify({
+        type: type,
+        info_id: info_id,
+        check_pass: check_pass,
+        check_conclusion: check_conclusion ? check_conclusion : null,
+      })
+      data = util.base64_encode(data)
+      var sign = util.sha1(data + timestamp + app.globalData.user_info.user_id)
+      //request
+      wx.request({
+        url: app.globalData.URL + 'manage/setCheckResult.php',
+        data: {
+          "version": 1,
+          "time": timestamp,
+          "data": data,
+          "sign": sign,
+          "token": app.globalData.token,
+        },
+        method: 'POST',
+        header: {
+          "content-type": "application/json"
+        },
+        success: res => {
+          console.log(res)
+          if (res.data.status == 1) return reject();
+          else return resolve();
+        }
+      })
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
 
   swiperTab: function (e) {
     var that = this;
--- call_board/pages/person/myissue/myissue.js
+++ call_board/pages/person/myissue/myissue.js
@@ -1,3 +1,5 @@
+var util = require('../../../utils/util.js');
+const app = getApp()
 Page({
 
   /**
@@ -5,40 +7,58 @@ Page({
    */
   data: {
     currentTab: 0,
-    SaveStatus: 'false', //收藏的状态
-    SaveNum: 10, //收藏的数量
-    ThumbStatus: 'false', //点赞的状态
-    ThumbNum: 20,//点赞的数量
+    boards: ['test'],//存放公告的数组
+    goods: [],//存放商品的数组 
+    end_goods_id:null,//已加载的最后一组物品数据，为null则没有了
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onReady: function (options) {
+    this.getGoodsItem(-1);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getGoodsItem:function(last_id){
+    //获取用户提交物品
+    var timestamp = Date.parse(new Date());
+    timestamp = String(timestamp / 1000);
+    var data = JSON.stringify({
+      summary_sub: 10,
+      count: 10,
+      from_id: last_id,
+      type: 0
+    })
+    data = util.base64_encode(data)
+    var sign = util.sha1(data + timestamp + app.globalData.user_info.user_id)
+    wx.request({
+      url: app.globalData.URL + 'user/getUserSubmit.php',
+      data: {
+        "version": 1,
+        "time": timestamp,
+        "data": data,
+        "sign": sign,
+        "token": app.globalData.token,
+      },
+      method: 'POST',
+      header: {
+        "content-type": "application/json"
+      },
+      success: res => {
+        if (res.data.status == 1) {
+          wx.showToast({
+            title: '获取数据失败',
+            icon: 'none',
+            duration: 4000
+          })
+          return;
+        }
+        res = JSON.parse(util.base64_decode(res.data.data))
+        this.data.goods.concat(res.goods_list)
+        this.setData({
+          goods: this.data.goods,
+          end_goods_id: res.end_goods_id
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
   swiperTab: function (e) {
     var that = this;
     that.setData({
